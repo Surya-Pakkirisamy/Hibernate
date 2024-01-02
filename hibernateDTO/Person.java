f84ac95d847ca40f3c093c9483c7d37c6f5e35c2
Surya-Pakkirisamy <suriya15062001@gmail.com>
2024-01-02 10:50:06 +0530

    updated with auto generation id with the help of @GeneratedValue(strategy = GenerationType.IDENTITY) annotation

--- hibernateDTO/Person.java
+++ hibernateDTO/Person.java
@@ -1,6 +1,8 @@
 package hibernateDTO;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
@@ -10,6 +12,7 @@ import javax.persistence.OneToOne;
 public class Person 
 {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int pId;
 	private String pName,pEmail;
 	@OneToOne
